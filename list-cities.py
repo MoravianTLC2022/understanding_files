24342ef40e8eb161b796434804539ecb52050447
Thomas Abato <abatot@moravian.edu>
2022-07-01 08:13:49 -0400

    Added user input to alter the file

--- list-cities.py
+++ list-cities.py
@@ -1,7 +1,10 @@
-file1 = open('cities.txt')
+#Opens the file to both amend and read
+file1 = open('cities.txt', 'a+')
+print('These are your current cities: ' + '\n')
+#Ensures regardless of the device that the text file will be found
+file1.seek(0)
+print(file1.read())
 
-for line in file1:
-    # remove the \n at the end of the line
-    line = line.strip()
-    print(line)
-
+new_city = input("Add a city to the file: ")
+file1.write('\n' + new_city)
+file1.close()
